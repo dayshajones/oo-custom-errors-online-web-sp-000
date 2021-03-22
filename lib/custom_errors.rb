700f3ab1d9247755bf3e2a5b353a45bd30a1a70e
Daysha Jones <daysha.vu@icloud.com>
2021-03-22 17:04:33 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -11,6 +11,8 @@ class Person
   end
   
   class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
   end
 
 end
